beda7b160c9898cc5a5287a44c69074088007fe9
NiravTalaviya <niravtalaviya011@gmail.com>
2021-10-16 14:01:04 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  double trial = 3.14;
+  String name = "TrialString";
+  bool isMale = true;
+  num temp = 30.4;
+  var lol = 5;
+  final pi = 3.14;
+  final pi2 = 3.15;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by ${name} $lol"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
